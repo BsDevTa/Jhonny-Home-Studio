226854fc3068625395fac3cc1ea5c551495d2400
BsDevTa <batistast.taua@gmail.com>
2026-06-28 00:04:36 -0300

    Projeto finalizado 2

--- src/apps/jhonny_home_studio_app/lib/core/config/api_config.dart
+++ src/apps/jhonny_home_studio_app/lib/core/config/api_config.dart
@@ -1,4 +1,5 @@
 class ApiConfig {
-  static const String apiOrigin = 'http://localhost:5299';
+  static const String apiOrigin =
+      'jhonny-home-studio-production.up.railway.app';
   static const String baseUrl = '$apiOrigin/api';
 }
